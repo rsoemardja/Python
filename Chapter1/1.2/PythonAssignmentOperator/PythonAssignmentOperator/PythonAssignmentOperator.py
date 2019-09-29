c1a0aae2da60a1ff7f6bab38a0c2cf377b2e8a4a
rsoemardja <rsoemardja@gmail.com>
2019-09-30 02:45:51 +1300

    Add files via upload

---
+++ Chapter1/1.2/PythonAssignmentOperator/PythonAssignmentOperator/PythonAssignmentOperator.py
@@ -0,0 +1,44 @@
+#This Module will Cover Assignment Operators Variables
+
+# x is the variable
+# = is the assignment operator (what it Equals to
+# 5 is the expression and will assign it to the variable
+x = 5
+
+# This Will print the value that is store in the variable x
+print(x)
+
+# This prints the letter, but not the value in the variable x
+print("x")
+
+# This is an assignment operator in math this is illegal (not allowed)
+# The x is the variable
+# The = is the assignment operator
+# and the x+1 is the mathmatical expression
+x = x+1
+# It will print x as the solution to the variable
+print (x)
+x+1
+
+# The value will not Change it will be stored variable
+print (x)
+
+# The Only way to change the Number in the varible is to add an expression to change the value
+#  x it will add a value to x to give the result
+x = x+1
+print(x)
+
+#This is not a valid expression because it not a vaild to assign it to a operator as a variable
+# The front HAS to be a variable 
+# you can flip it math but the the rules in programming are explicit in this context
+x+1 = x
+
+# This Expression and the Second Expression are equivalent to each other
+x = x+1
+print (x)
+
+x += 1
+print (x)
+# you can minus in the equaltion and it is equvalent as well
+x -= 3
+print (x)
